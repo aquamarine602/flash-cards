8139fe6bd7d944d27449d3e4b6ed2b06a091f17c
chloe.spilker <chloe.spilker@CLC-LAB209-13.TCSD.tooelesd.org>
2019-01-03 14:08:30 -0700

    sajdf

--- src/flash_cards/Quiz_card_player.java
+++ src/flash_cards/Quiz_card_player.java
@@ -5,6 +5,10 @@ package flash_cards;
 import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
+
+import flash_cards.Quiz_card_builder.New_menu_listener;
+import flash_cards.Quiz_card_builder.Save_menu_listener;
+
 import java.awt.*;
 import java.io.*;
 
@@ -31,18 +35,36 @@ public class Quiz_card_player {
 		JPanel main_pan = new JPanel();
 		Font big_font = new Font("sanserif", Font.BOLD, 24);
 		
+		// displays question
 		display = new JTextArea(6, 20);
 		display.setLineWrap(true);
-		display.setEditable(false);
+		display.setEditable(false);		// ONLY displays question
 		display.setFont(big_font);
 		
+		// scroller for display
 		JScrollPane q_scroller = new JScrollPane(display);
 		q_scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		q_scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		
+		// next button
 		next_b = new JButton("Show Question");
 		main_pan.add(q_scroller);
 		main_pan.add(next_b);
+		//nextButton.addActionListener(new Next_card_listener());
+		
+		// menu bar to load in a deck of cards
+		JMenuBar menu_bar = new JMenuBar();
+		JMenu file_menu = new JMenu("File");
+		JMenuItem load_menu_item = new JMenuItem("Load");
+		//load_menu_item.addActionListener(new Open_menu_listener());
+		file_menu.add(load_menu_item);
+		menu_bar.add(file_menu);
+		
+		// set frame stuff
+		frame.setJMenuBar(menu_bar);
+		frame.getContentPane().add(BorderLayout.CENTER, main_pan);
+		frame.setSize(640, 500);
+		frame.setVisible(true);
 	}
 
 }
