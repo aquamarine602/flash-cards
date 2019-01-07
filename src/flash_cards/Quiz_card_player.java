2134609017aa1820b0e3e01fb37ad4a82e0b41ab
chloe.spilker <chloe.spilker@CLC-LAB209-13.TCSD.tooelesd.org>
2019-01-07 13:43:12 -0700

    vds

--- src/flash_cards/Quiz_card_player.java
+++ src/flash_cards/Quiz_card_player.java
@@ -3,6 +3,7 @@ package flash_cards;
 // a program that reads cards from a saved list
 
 import java.util.*;
+
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -12,13 +13,15 @@ import flash_cards.Quiz_card_builder.Save_menu_listener;
 import java.awt.*;
 import java.io.*;
 
+@SuppressWarnings("unused")
 public class Quiz_card_player {
 	
 	private JTextArea display;
+	@SuppressWarnings("unused")
 	private JTextArea answer;
 	private ArrayList<Quiz_card>card_list;
 	private Quiz_card current_card;
-	private int current_card_cndex;
+	private int current_card_index;
 	private JFrame frame;
 	private JButton next_b;
 	private boolean is_show_answer;
@@ -50,21 +53,78 @@ public class Quiz_card_player {
 		next_b = new JButton("Show Question");
 		main_pan.add(q_scroller);
 		main_pan.add(next_b);
-		//nextButton.addActionListener(new Next_card_listener());
+		next_b.addActionListener(new Next_card_listener());
 		
 		// menu bar to load in a deck of cards
 		JMenuBar menu_bar = new JMenuBar();
 		JMenu file_menu = new JMenu("File");
 		JMenuItem load_menu_item = new JMenuItem("Load");
-		//load_menu_item.addActionListener(new Open_menu_listener());
+		load_menu_item.addActionListener(new Open_menu_listener());
 		file_menu.add(load_menu_item);
 		menu_bar.add(file_menu);
 		
 		// set frame stuff
 		frame.setJMenuBar(menu_bar);
 		frame.getContentPane().add(BorderLayout.CENTER, main_pan);
-		frame.setSize(640, 500);
+		frame.setSize(500, 300);
 		frame.setVisible(true);
+	}
+	
+	public class Next_card_listener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0) {
+			if (is_show_answer) {
+				display.setText(current_card.getAnswer());
+				next_b.setText("Next Card");
+				is_show_answer = false;
+			} else {
+				if  (current_card_index < card_list.size()) {
+					show_next_card();
+				} else {
+					display.setText("That was the last card.");
+					next_b.setEnabled(false);
+				}
+			}
+		}
+	}
+	
+	public class Open_menu_listener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			JFileChooser fileOpen = new JFileChooser();
+			fileOpen.showOpenDialog(frame);
+			load_file(fileOpen.getSelectedFile());
+			
+		}
+	}
+	
+	private void show_next_card() {
+		current_card = card_list.get(current_card_index);
+		current_card_index++;
+		display.setText(current_card.getQuestion());
+		is_show_answer = true;
+	}
+	
+	private void load_file(File file) {
+		card_list = new ArrayList<Quiz_card>();
+		
+		try {
+			@SuppressWarnings("resource")
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				make_card(line);
+			} 
+		} catch (Exception ex) {
+			System.out.println("Couldn't read the file.");
+			ex.printStackTrace();
+		}
+		show_next_card();
+	}
+	
+	private void make_card(String line_to_parse) {
+		String[] results = line_to_parse.split("/");
+		Quiz_card card = new Quiz_card(results[0], results[1]);
+		card_list.add(card);
+		System.out.println("Card made.");
 	}
 
 }
